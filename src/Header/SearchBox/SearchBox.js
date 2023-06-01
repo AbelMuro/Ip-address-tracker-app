ac5e4af80e604555389dc8090e2520c67d01aab0
abel muro <abelmuro93@gmail.com>
2023-05-31 21:48:01 -0700

    commit

--- src/Header/SearchBox/SearchBox.js
+++ src/Header/SearchBox/SearchBox.js
@@ -1,6 +1,7 @@
 import React, {useState, useRef} from 'react';
 import styles from './styles.module.css';
 import icons from './icons';
+//need to install redux
 
 function SearchBox () {
     const [query, setQuery] = useState('');
@@ -8,6 +9,9 @@ function SearchBox () {
     const errorMessage = useRef();
 
     const handleChange = (e) => {
+        searchBox.current.style.border = '';
+        errorMessage.current.style.display = '';
+        searchBox.current.setCustomValidity('');
         setQuery(e.target.value);
     }
 
@@ -18,11 +22,16 @@ function SearchBox () {
             searchBox.current.style.border = '';
             errorMessage.current.style.display = '';
         }
-            
         else{
             searchBox.current.style.border = '2px solid red';
             errorMessage.current.style.display = 'block'
         }
+    }
+
+    const handleInvalid = (e) => {
+        e.target.setCustomValidity(' ');
+        searchBox.current.style.border = '2px solid red';
+        errorMessage.current.style.display = 'block'
     }
 
     const handleSubmit = () => {
@@ -30,12 +39,13 @@ function SearchBox () {
     }
 
     return (
-        <form className={styles.container} onClick={handleSubmit}>
+        <form className={styles.container} onSubmit={handleSubmit}>
             <input 
                 value={query} 
                 placeholder='Search for any IP address or domain'
                 onChange={handleChange} 
                 onBlur={handleBlur}
+                onInvalid={handleInvalid}
                 className={styles.searchBox}
                 ref={searchBox}
                 required
